6a4d4d74c9f51c62955dc9d7c7ccd06121b0b7ad
Urriiyy <116164082+Urriiyy@users.noreply.github.com>
2022-10-20 12:07:50 +0300

    Update collection2.kt

--- src/main/kotlin/collections/collection2.kt
+++ src/main/kotlin/collections/collection2.kt
@@ -1,17 +1,16 @@
 // Создание пустой коллекции (collection), наполнение значениями c консоли, вывод содержимого в консоль.
 
 //
-
 fun main() {
 
-    val collection: ArrayList<String> = arrayListOf() // создание пустой коллекции
+    val collection: ArrayList<Char> = arrayListOf() // создание пустой коллекции
     var enter: String // для ввода из консоли
 
     while (true){ // зааполнение коллекции
-        println("\nДобавьте элемент в список или для завершения введите \"end\"")
+        println("\nДобавьте символ в список или для завершения введите \"end\"")
         enter = readLine().toString()
         if (enter == "end") break // проверка завершения заполнения
-        collection.add(enter)
+        collection.add(enter[0])
     }
 
     println("\nКолличество элементов в списке ${collection.size} \n")
